7cbb37d22d8fe7d7fa693bd92582abd11aeb644e
learGitHub <imintseng6@gmail.com>
2019-07-17 19:57:27 +0000

    Flixter_25.Enrollment & Payments_Permissions.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :student_enrolled!
+
   def show
+
   end 
   
   private
+
+  def student_enrolled!
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to course_path(current_lesson.section.course), alert: 'You need to be enrolled in thi course!'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
