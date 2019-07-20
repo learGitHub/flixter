a25615755fefd6f566d8c4ba78823078313657e0
learGitHub <imintseng6@gmail.com>
2019-07-20 21:43:25 +0000

    Flixter_33.Finishing Touches_New lesson form modal.

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -18,7 +18,8 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
-   @section = Section.new
+    @section = Section.new
+    @lesson = Lesson.new
   end
 
   private
